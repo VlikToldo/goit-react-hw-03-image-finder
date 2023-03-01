f243b9ea31ec81037011028b876e0691939a80ef
VlikToldo <108451651+VlikToldo@users.noreply.github.com>
2023-03-01 18:24:41 +0300

    1

--- src/components/modules/ImageFinder/ImageDetails/ImageDetails.jsx
+++ src/components/modules/ImageFinder/ImageDetails/ImageDetails.jsx
@@ -4,7 +4,7 @@ import style from './image-details.module.css'
 
 const ImageDetails = ({imageDetails}) => {
    return <div>
-        <img className={style.Image} src={imageDetails} alt='`image`' />
+        <img className={style.Image} src={imageDetails}  />
     </div>
 }
 
